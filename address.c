19c2e6e1edd15ca14255c377a36ea32f42b546a4
Blessed Oigbochie <eprince1800@gmail.com>
2023-08-23 17:14:24 +0100

    reversed string

--- address.c
+++ address.c
@@ -8,3 +8,16 @@ void address(void *a)
 	if (char_ptr != NULL)
 		paste(*char_ptr);
 }
+
+void rev_str(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	while (i >= 0)
+	{
+		paste(str[i]);
+		i--;
+	}
+}
